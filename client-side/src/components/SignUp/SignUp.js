c8cec66115fbda6bbe19eeff5b96c00211d29bf1
EnricoValastro <79710717+EnricoValastro@users.noreply.github.com>
2022-12-30 11:45:36 +0100

    SignupValidateField1.0

--- client-side/src/components/SignUp/SignUp.js
+++ client-side/src/components/SignUp/SignUp.js
@@ -14,20 +14,7 @@ function Signup(){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
-    function NameHandler(event){
-        if(event.target.value.length >= 3){
-            document.getElementById('errorBoxName').classList.add('hidden');
-            setName(event.target.value)
-        }
-        else{
-            document.getElementById('errorBoxName').classList.remove('hidden');
-        }
 
-
-    }
-    function SurnameHandler(event){
-        setSurname(event.target.value)
-    }
     function EmailHandler(event){
         setEmail(event.target.value)
     }
@@ -73,8 +60,60 @@ function Signup(){
         }
     }
 
-    function nextInput(){
+    function nameNextInput(){
+        const nameInput = document.getElementById('nameField');
+        const nextInput = document.getElementById('surnameInput');
+        if(nameInput.value.length >= 1){
+            setName(nameInput.value);
+            document.getElementById('errorBoxName').classList.add('hidden');
+            nextInput.classList.remove('hidden');
+        }
+        else{
+            document.getElementById('errorBoxName').classList.remove('hidden');
+        }
+    }
 
+    function surnameNextInput(){
+        const surnameInput = document.getElementById('surnameField');
+        const nextInput = document.getElementById('emailInput');
+        if(surnameInput.value.length >= 1){
+            setSurname(surnameInput.value);
+            document.getElementById('errorBoxSurname').classList.add('hidden');
+            nextInput.classList.remove('hidden');
+        }
+        else{
+            document.getElementById('errorBoxSurname').classList.remove('hidden');
+        }
+    }
+
+    function validateEmail(email) {
+       axios.post("http://localhost:4000/api/emailValidation", {
+              email: email
+       }).then((response)=> {
+           if (response.data) {
+               return false;
+           } else {
+               return true;
+           }
+       });
+    }
+
+
+
+    function emailNextInput(){
+        const emailInput = document.getElementById('emailField').value;
+        const nextInput = document.getElementById('pwdInput');
+        if(emailInput == ''){
+            document.getElementById('errorBoxEmail').classList.remove('hidden');
+        }
+        else if(/^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(emailInput) && validateEmail(emailInput)){
+            document.getElementById('errorBoxEmail').classList.add('hidden');
+            setEmail(emailInput);
+            nextInput.classList.remove('hidden');
+        }
+        else{
+            document.getElementById('errorBoxEmail').classList.remove('hidden');
+        }
     }
 
     return (
@@ -103,25 +142,33 @@ function Signup(){
                         </div>
                         <div className="form">
                             <form onSubmit={SubmitHandler}>
+
                                 <div id="nameInput"className="inputContainer">
                                     <p className="enterSomething">Enter your name</p>
                                     <div className="xxx">
-                                        <input className="input" type="text" onBlur={NameHandler} />
-                                        <button onClick={nextInput}>-></button>
+                                        <input id="nameField"className="input" type="text" onBlur={nameNextInput}/>
+                                        <button onClick={nameNextInput}>→</button>
                                     </div>
 
-
-                                    <div className="errorMsg hidden" id="errorBoxName">
-                                        <p>Name must be at least 3 characters long</p>
-                                    </div>
                                 </div>
+
                                 <div id="surnameInput" className="inputContainer hidden">
                                     <p className="enterSomething">Enter your surname</p>
-                                    <input className="input" type="text" onBlur={SurnameHandler}/>
+                                    <div className="xxx">
+                                        <input id="surnameField" className="input" type="text" onBlur={surnameNextInput}/>
+                                        <button onClick={surnameNextInput}>→</button>
+                                    </div>
+
                                 </div>
+
                                 <div id="emailInput" className="inputContainer hidden">
                                     <p className="enterSomething">Enter your email</p>
-                                    <input className="input" type="email" onBlur={EmailHandler}/>
+                                    <div className="xxx">
+                                        <input id="emailField" className="input" type="email" onBlur={emailNextInput}/>
+                                        <button onClick={emailNextInput}>→</button>
+                                    </div>
+
+
                                 </div>
                                 <div id="pwdInput" className="inputContainer hidden">
                                     <p className="enterSomething">Enter your password</p>
@@ -138,6 +185,16 @@ function Signup(){
                             </form>
                         </div>
                     </div>
+                    <div className="errorMsg hidden" id="errorBoxName">
+                        <p>Name must be at least 1 characters long</p>
+                    </div>
+                    <div className="errorMsg hidden" id="errorBoxSurname">
+                        <p>Surname must be at least 1 characters long</p>
+                    </div>
+                    <div className="errorMsg hidden" id="errorBoxEmail">
+                        <p>Email is invalid or already taken</p>
+                    </div>
+
 
                 </div>
 
