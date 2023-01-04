d468da63d6078fc1917183f85a31519f371b4849
EnricoValastro <79710717+EnricoValastro@users.noreply.github.com>
2023-01-04 12:47:48 +0100

    NotFoundPageFixed

--- client-side/src/components/NotFoundComponent/NotFoundComponent.js
+++ client-side/src/components/NotFoundComponent/NotFoundComponent.js
@@ -6,29 +6,29 @@ export default function NotFoundComponent() {
     return(
         <div>
 
-            <div class="bg-purple">
+            <div className="bg-purple">
 
-            <div class="stars">
+            <div className="stars">
 
-                <div class="central-body">
-                    <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px"></img>
+                <div className="central-body">
+                    <img className="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px"></img>
                 </div>
-                <div class="objects">
-                    <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"></img>
-                    <div class="earth-moon">
-                        <img class="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"></img>
-                        <img class="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px"></img>
+                <div className="objects">
+                    <img className="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px"></img>
+                    <div className="earth-moon">
+                        <img className="object_earth" src="http://salehriaz.com/404Page/img/earth.svg" width="100px"></img>
+                        <img className="object_moon" src="http://salehriaz.com/404Page/img/moon.svg" width="80px"></img>
                     </div>
-                    <div class="box_astronaut">
-                        <img class="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px"></img>
+                    <div className="box_astronaut">
+                        <img className="object_astronaut" src="http://salehriaz.com/404Page/img/astronaut.svg" width="140px"></img>
                     </div>
                 </div>
-                <div class="glowing_stars">
-                    <div class="star"></div>
-                    <div class="star"></div>
-                    <div class="star"></div>
-                    <div class="star"></div>
-                    <div class="star"></div>
+                <div className="glowing_stars">
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
+                    <div className="star"></div>
 
                 </div>
 
